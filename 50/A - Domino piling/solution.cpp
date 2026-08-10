794d89cb26f6311c61db1c1cad718502e79173ff
Himanshu Kumar <himanshukumar4736@gmail.com>
2026-08-10 18:16:55 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+ 
+void domino_piling(){
+    int m{}, n{};
+    cin >> m >> n;
+    cout << (m*n)/2 << endl;
+}
+ 
+int main(){
+    domino_piling();
+    return 0;
+}
